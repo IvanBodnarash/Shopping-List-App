1330abf389bba530d14a6633252efc5789f6b2fb
Ivan Bodnarash <bodnarash1999@gmail.com>
2024-04-04 12:40:58 -0600

    sending http requests and fetching & transforming data

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
 
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
@@ -11,21 +17,45 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+      'flutter-apps-test-ba4da-default-rtdb.firebaseio.com', 'shopping-list.json');
+    final response = await http.get(url);
+    final Map<String, dynamic> listdata = json.decode(response.body);
+    final List<GroceryItem> _loadedItems = [];
+    for (final item in listdata.entries) {
+      final category = categories.entries.firstWhere((catItem) => catItem.value.title == item.value['category']).value;
+      _loadedItems.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        )
+      );
+    }
+
+    setState(() {
+      _groceryItems = _loadedItems;
+    });
+  }
+
   void _addItem() async {
-    final newItem =await Navigator.of(context).push<GroceryItem>(
+    await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItem(),
       ),
     );
 
-    if (newItem == null) {
-      return;
-    }
-
-    setState(() {
-      _groceryItems.add(newItem);
-    });
+    _loadItems();
   }
 
   void _removeItem(GroceryItem item) {
@@ -63,12 +93,15 @@ class _GroceryListState extends State<GroceryList> {
       );
     }
     return Scaffold(
-      appBar: AppBar(title: const Text('Grocery List'), actions: [
-        IconButton(
-          onPressed: _addItem,
-          icon: const Icon(Icons.add),
-        )
-      ],),
+      appBar: AppBar(
+        title: const Text('Grocery List'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
       body: content,
     );
   }
